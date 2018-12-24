45f6f187fd133e2ec525613032a3aa7e9cc4257c
leeha12 <leeyuval12@gmail.com>
2018-12-24 15:14:32 +0200

    Merge branch 'master' of https://github.com/rotemb88/pro-rl
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/res/layout/activity_main.xml
    #	build.gradle

--- app/src/main/java/com/eran/myapp/Login.java
+++ app/src/main/java/com/eran/myapp/Login.java
@@ -1,13 +1,31 @@
 package com.eran.myapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
 
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login=(Button)findViewById(R.id.ok2button);
+
+        login.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v==login)
+        {
+            startActivity(new Intent(Login.this,menu.class));
+
+        }
+    }
+
 }
--- app/src/main/java/com/eran/myapp/register.java
+++ app/src/main/java/com/eran/myapp/register.java
@@ -1,13 +1,31 @@
 package com.eran.myapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class register extends AppCompatActivity {
+public class register extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mainact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mainact=(Button)findViewById(R.id.okbutton);
+
+        mainact.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v==mainact)
+        {
+            startActivity(new Intent(register.this,MainActivity.class));
+        }
     }
 }
