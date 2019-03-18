b09aa871b0ed729854778b0d8068f69984529b6b
rotemb88 <rotemb88@gmail.com>
2019-03-18 16:30:24 +0200

    Merge branch 'master' of https://github.com/rotemb88/pro-rl
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/eran/myapp/menu.java
+++ app/src/main/java/com/eran/myapp/menu.java
@@ -1,13 +1,27 @@
 package com.eran.myapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class menu extends AppCompatActivity {
+public class menu extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton tipimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        tipimage = (ImageButton) findViewById(R.id.imageButton);
+        tipimage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(menu.this,tips.class));
+
     }
 }
